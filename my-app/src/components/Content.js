c82f597a30bc87b4734d995a1e8722256b13af7f
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-01-16 16:39:00 +0000

    Sixth challenge completed

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,8 +2,31 @@ import React, { Component } from 'react'
 import css from "./css/Content.module.css";
 import {savedPosts} from "../posts.json";
 import PostItem from './PostItem';
+import Loader from './Loader';
 
 export class Content extends Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            isLoaded: false
+        }
+    }
+
+    getData() {
+        console.log("getData called")
+        setTimeout(() => {
+            console.log("Timeout called")
+            this.setState({
+                isLoaded: true,
+            })
+        }, 2000)
+    }
+
+    componentDidMount() {
+        console.log("Component has mounted")
+        this.getData()
+    }
 
     render() {
         return (
@@ -12,7 +35,11 @@ export class Content extends Component {
                     <h1>My Photos</h1>
                 </div>
                 <div className={css.SearchResults}>
-                    <PostItem savedPosts={savedPosts} />
+                    {
+                        this.state.isLoaded ? 
+                        <PostItem savedPosts={savedPosts} />
+                        : <Loader />
+                    }
                 </div>
             </div>
         )
