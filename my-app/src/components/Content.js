91b15c8f29732befdf2c387e885fb573fc7785d8
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-01-17 13:13:14 +0000

    Seventh challenge completed

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -9,35 +9,51 @@ export class Content extends Component {
         super(props)
         
         this.state = {
-            isLoaded: false
+            isLoaded: false,
+            posts: [],
         }
     }
 
-    getData() {
-        console.log("getData called")
-        setTimeout(() => {
-            console.log("Timeout called")
+    componentDidMount() {
+        setTimeout(()=>{
             this.setState({
                 isLoaded: true,
+                posts: savedPosts,
             })
         }, 2000)
     }
 
-    componentDidMount() {
-        console.log("Component has mounted")
-        this.getData()
+    handleSearch = (event) => {
+        let author = event.target.value.toLowerCase();
+        let filteredPosts = savedPosts.filter((post)=>{
+            return post.name.toLowerCase().includes(author);
+        })
+
+        this.setState({
+            posts:filteredPosts
+        })
     }
+
 
     render() {
         return (
             <div className={css.Content}>
                 <div className={css.TitleBar}>
                     <h1>My Photos</h1>
+                    <form>
+                        <label htmlFor='searchInput'>Search:</label>
+                        <input 
+                            type='search' 
+                            id='searchInput' 
+                            placeholder='By Author'
+                            onChange={this.handleSearch}/>
+                    </form>
+                    <h4>posts found: {this.state.posts.length}</h4>
                 </div>
                 <div className={css.SearchResults}>
                     {
                         this.state.isLoaded ? 
-                        <PostItem savedPosts={savedPosts} />
+                        <PostItem savedPosts={this.state.posts} />
                         : <Loader />
                     }
                 </div>
